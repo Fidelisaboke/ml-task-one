7314d12f5568c77633fbdc872dfe6ecf0db153f7
fidelisaboke <fidelisaboke@gmail.com>
2024-10-25 13:34:13 +0300

    Update MSE function

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def mean_squared_error(y_true, y_pred):
-    return np.sum((y_true - y_pred) ** 2) / len(y_true)
+    return np.mean((y_true - y_pred) ** 2)
 
 
 def gradient_descent(x, y, w, b, learning_rate=0.0001):
